ee4b3e489894915c9c44371db7ab19dcb92d51dc
OryJonay <kriegsmeister@gmail.com>
2014-11-19 12:14:43 +0200

    Another indent

--- AutoCompModule.py
+++ AutoCompModule.py
@@ -123,12 +123,12 @@ class AutoCompModule:
                 if a is not None:
                     biScore += (self.dictBy2.find_one({"first": prev, "second": word})["grade"])/(self.dictBy2.find_one({"first": prev, "second": a})["grade"])
                     biChecks += 1
-                    if b is not None:
-                        triScore += (self.dictBy3.find_one({"first": pprev, "second": prev, "third": word})["grade"])/(self.dictBy3.find_one({"first": pprev, "second": prev, "third": b})["grade"])
-                        triChecks += 1
-                    i=num
-                    pprev=prev
-                    prev=word
+                if b is not None:
+                    triScore += (self.dictBy3.find_one({"first": pprev, "second": prev, "third": word})["grade"])/(self.dictBy3.find_one({"first": pprev, "second": prev, "third": b})["grade"])
+                    triChecks += 1
+                i=num
+                pprev=prev
+                prev=word
         test.close()
         return biScore/biChecks, triScore/triChecks
 
