d8aa1ee43afc462537bf120b18ae8400e29682ee
OryJonay <kriegsmeister@gmail.com>
2014-11-21 13:17:13 +0200

    Added comments to AutoCompModule All the testing methods are now in TestingForACM

--- AutoCompModule.py
+++ AutoCompModule.py
@@ -3,11 +3,22 @@ from pymongo import Connection
 
 class AutoCompModule:
 
+    # Auto completion module
+    # Using the MongoDB server
+    # Holds three dictionaries :
+    #   dict - holds the amount of x's appearances in the learned text
+    #   dictBy2 - holds the amount of (x,y) appearances in the learned text
+    #   dictBy2 - holds the amount of (x,y,z) appearances in the learned text
     def __init__(self,DBName):
         self.dict = Connection()[DBName]['dict']
         self.dictBy2 = Connection()[DBName]['dictBy2']
         self.dictBy3 = Connection()[DBName]['dictBy3']
     
+    
+    # Method to learn from a single file
+    # For each file the method detects all the information mentioned above
+    # Definitions :
+    #   pprev,prev,word are the three last seen words (where word is the current word) 
     def learnSingle(self,fileName):
         input = open(fileName, encoding='utf-8')
         for line in input:
@@ -17,10 +28,10 @@ class AutoCompModule:
                     pprev = prev = word = None
                     continue
             
-                if self.dict.find_one({"word": word,"amount": { "$exists": True}}) != None:
-                    self.dict.update({"word": word},{ "$inc": {"amount":1}})
+                if self.dict.find_one({"word": word,"grade": { "$exists": True}}) != None:
+                    self.dict.update({"word": word},{ "$inc": {"grade":1}})
                 else:
-                    self.dict.insert({"word": word, "amount":1})
+                    self.dict.insert({"word": word, "grade":1})
             
                 if prev!=None:
                     if self.dictBy2.find_one({"first": prev,"second": word,"grade": { "$exists": True}}) != None:
@@ -36,6 +47,9 @@ class AutoCompModule:
                 prev = word
         input.close()
 
+
+    # Method to learn from multiple files
+    # Uses learnSingle Method
     def learn(self,inputDir):
         if os.path.isdir(inputDir):
             for f in sorted(os.listdir(inputDir)):
@@ -45,6 +59,14 @@ class AutoCompModule:
         else:
             print ("ERROR!!")
 
+
+    # Method that suggests the next word
+    # For a given pprev and prev (definitions mentioned above) it finds the most likely word, one time
+    # using only prev and the second using both pprev and prev
+    # 
+    # This method returns both NONE and NOT NONE values
+    # None values are returned when there is no match to prev (or pprev and prev) in the dictionaries 
+    # or when they are gives as NONE
     def suggest(self,pprev=None,prev=None):
         if prev is None:
             return None , None
@@ -59,97 +81,6 @@ class AutoCompModule:
         if b is not None:
             return a["second"] , b["third"]
         else:
-            return a["second"] , None
+            return None , None
     
-    def simpleTestSingle(self, testFile, num):
-        test = open(testFile,'r',encoding='utf-8')
-        numOfChecks1 = numOfChecks2 = succ1 = succ2 = 0
-        i = num
-        for line in test:
-            pprev = prev = None
-            for word in line.split():
-                if re.match("[.,\"\(\);']",word):
-                    pprev = prev = word = None
-                    i = num
-                    continue
-                if i!= 0:
-                    i-=1
-                    pprev = prev
-                    prev = word
-                else:
-                    a,b = self.suggest(pprev,prev)
-                    if a is not None:
-                        if a is word:
-                            succ1+=1
-                        numOfChecks1+=1
-                    if b is not None:
-                        if b is word:
-                            succ2+=1
-                        numOfChecks2+=1
-                    i=num
-                    pprev=prev
-                    prev=word
-        test.close()
-        return succ1/numOfChecks1, succ2/numOfChecks2
 
-    def simpleTest(self,inputDir,num):
-        sum1 = sum2 = 0
-        numOfFiles = 0
-        if os.path.isdir(inputDir):
-            for f in sorted(os.listdir(inputDir)):
-                x1,x2 = self.simpleTestSingle(inputDir + '/' + f,num)
-                sum1+=x1
-                sum2+=x2
-                numOfFiles+=1
-            return sum1/numOfFiles, sum2/numOfFiles
-        return "input Error"
-    
-    def probTestSingle(self, testFile, num):    
-        test = open(testFile,'r',encoding='utf-8')
-        biScore = triScore = 0.0
-        biChecks = triChecks = 0
-        i = num
-        for line in test:
-            pprev = prev = None
-            for word in line.split():
-                if re.match("[.,\"\(\);']",word):
-                    pprev = prev = word = None
-                    i = num
-                    continue
-                if i != 0:
-                    i -= 1
-                    pprev = prev
-                    prev = word
-                else:
-                    a,b = self.suggest(pprev,prev)
-                    c = self.dictBy2.find_one({"first": prev, "second": word})
-                    d = self.dictBy3.find_one({"first": pprev, "second": prev, "third": word}
-                    if a is not None:
-                        if c is not None:
-                            biScore += (c["grade"])/(self.dictBy2.find_one({"first": prev, "second": a})["grade"])
-                            biChecks += 1
-                        else:
-                            biScore = biScore
-                    if b is not None:
-                        if d is not None:
-                            triScore += (d["grade"])/(self.dictBy3.find_one({"first": pprev, "second": prev, "third": b})["grade"])
-                            triChecks += 1  
-                        else:
-                            triScore = triScore
-                    i=num
-                    pprev=prev
-                    prev=word
-        test.close()
-        return biScore/biChecks, triScore/triChecks
-
-    def probTest(self,inputDir,num):
-        sum1 = sum2 = 0
-        numOfFiles = 0
-        if os.path.isdir(inputDir):
-            for f in sorted(os.listdir(inputDir)):
-                x1,x2 = self.probTestSingle(inputDir + '/' + f,num)
-                sum1+=x1
-                sum2+=x2
-                numOfFiles+=1
-            return sum1/numOfFiles, sum2/numOfFiles
-        return "input Error"
---
+++ TestingForACM.py
@@ -0,0 +1,122 @@
+import AutoCompModule
+from AutoCompModule import AutoCompModule
+import pymongo, os, sys, re
+
+# Testing for the AutoCompModule
+
+
+def simpleTestSingle(self, testFile, num):
+        test = open(testFile,'r',encoding='utf-8')
+        numOfChecks1 = numOfChecks2 = succ1 = succ2 = 0
+        i = num
+        for line in test:
+            pprev = prev = None
+            for word in line.split():
+                if re.match("[.,\"\(\);']",word):
+                    pprev = prev = word = None
+                    i = num
+                    continue
+                if i!= 0:
+                    i-=1
+                    pprev = prev
+                    prev = word
+                else:
+                    a,b = self.suggest(pprev,prev)
+                    if a is not None:
+                        if a is word:
+                            succ1+=1
+                        numOfChecks1+=1
+                    if b is not None:
+                        if b is word:
+                            succ2+=1
+                        numOfChecks2+=1
+                    i=num
+                    pprev=prev
+                    prev=word
+        test.close()
+        return succ1/numOfChecks1, succ2/numOfChecks2
+
+def simpleTest(self,inputDir,num):
+        sum1 = sum2 = 0
+        numOfFiles = 0
+        if os.path.isdir(inputDir):
+            for f in sorted(os.listdir(inputDir)):
+                x1,x2 = simpleTestSingle(self,inputDir + '/' + f,num)
+                sum1+=x1
+                sum2+=x2
+                numOfFiles+=1
+            return sum1/numOfFiles, sum2/numOfFiles
+        return "input Error"
+    
+def probTestSingle(self, testFile, num):    
+        test = open(testFile,'r',encoding='utf-8')
+        biScore = triScore = 0.0
+        biChecks = triChecks = 0
+        i = num
+        for line in test:
+            pprev = prev = None
+            for word in line.split():
+                if re.match("[.,\"\(\);']",word):
+                    pprev = prev = word = None
+                    i = num
+                    continue
+                if i != 0:
+                    i -= 1
+                    pprev = prev
+                    prev = word
+                else:
+                    a,b = self.suggest(pprev,prev)
+                    c = self.dictBy2.find_one({"first": prev, "second": word})
+                    d = self.dictBy3.find_one({"first": pprev, "second": prev, "third": word})
+                    if a is not None:
+                        if c is not None:
+                            biScore += (c["grade"])/(self.dictBy2.find_one({"first": prev, "second": a})["grade"])
+                        else:
+                            biScore = biScore
+                        biChecks += 1
+                    if b is not None:
+                        if d is not None:
+                            triScore += (d["grade"])/(self.dictBy3.find_one({"first": pprev, "second": prev, "third": b})["grade"])
+                        else:
+                            triScore = triScore
+                        triChecks += 1
+                    i=num
+                    pprev=prev
+                    prev=word
+        test.close()
+        return biScore/biChecks, triScore/triChecks
+
+def probTest(self,inputDir,num):
+        sum1 = sum2 = 0
+        numOfFiles = 0
+        if os.path.isdir(inputDir):
+            for f in sorted(os.listdir(inputDir)):
+                x1,x2 = probTestSingle(self,inputDir + '/' + f,num)
+                sum1+=x1
+                sum2+=x2
+                numOfFiles+=1
+            return sum1/numOfFiles, sum2/numOfFiles
+        return "input Error"
+
+# Input for the testing::
+#   1 - the database name we want to check
+#   2 - the directory we want to test
+#   3 - the test type
+#   4 - num of checks
+def main():
+    DBname = sys.argv[1]
+    Indir = sys.argv[2]
+    TestType = sys.argv[3]
+    numOfChecks = int(sys.argv[4])
+
+    DB = AutoCompModule(DBname)
+    
+    if TestType == 's':
+        print (simpleTest(DB,Indir,numOfChecks))
+    else:
+        print (probTest(DB,Indir,numOfChecks))
+
+
+if __name__ == '__main__':
+    print ("Working\n")
+    main()
